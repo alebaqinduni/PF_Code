b618c05c4eb4f05bdc1c6498bcdd545f6bded9a9
alebaqinduni <alebaqinduni@gmail.com>
2026-03-13 01:33:54 +0500

    Added new files

--- lab/week4/hometask8.cpp
+++ lab/week4/hometask8.cpp
@@ -1,26 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
-    int num;
+    int number;
     cout<<"Enter a number(0-100): ";
-    cin>>num;
-    string words[]={"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
-    string teens[]={"Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-    string tens[]={"","", "Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
-    string word;
-    if(num<10){
-        cout<<words[num]<<endl;
-    }
-    else if(num<20){
-        cout<<teens[num-10]<<endl;
-    }
-    else if(num<100){cout<<tens[num/10]<<(num%10 ? "-"+words[num%10] : "")<<endl;
-    }
-    else if(num==100){
-        cout<<"One Hundred"<<endl;
-    }
-    else{
-        cout<<"Number out of range!"<<endl;
+    cin>>number;
+    int ones=number % 10;
+    int teens=number / 10;
+    int tens=number / 10;
+    cout<<"Teens: "<<teens<<endl;
+    cout<<"Ones: "<<ones<<endl;
+    if (number>=20 && number<=100){
+        if(number==100){cout<<" Hundred";}
+        if(tens==9){cout<<" Ninety";}
+      if(tens==8){cout<<" Eighty";}
+      if(tens==7){cout<<" Seventy";}
+      if(tens==6){cout<<" Sixty";}
+      if(tens==5){cout<<" Fifty";}
+      if(tens==4){cout<<" Forty";}
+    if(tens==3){cout<<" Thirty";}
+      if(tens==2){cout<<" Twenty";}
+      if(tens==1){cout<<" Ten";}
+      if(teens==19){cout<<" Nineteen";}
+    if(teens==18){cout<<" Eighteen";}
+      if(teens==17){cout<<" Seventeen";}
+      if(teens==16){cout<<" Sixteen";}
+      if(teens==15){cout<<" Fifteen";}
+      if(teens==14){cout<<" Fourteen";}
+      if(teens==13){cout<<" Thirteen";}
+      if(teens==12){cout<<" Twelve";}
+      if(teens==11){cout<<" Eleven";}
+      if(teens==10){cout<<" Ten";}
+if(ones==9){cout<<" Nine";}
+if(ones==8){cout<<" Eight";}
+if(ones==7){cout<<" Seven";}
+if(ones==6){cout<<" Six";}
+if(ones==5){cout<<" Five";}
+if(ones==4){cout<<" Four";}
+if(ones==3){cout<<" Three";}
+if(ones==2){cout<<" Two";}
+if(ones==1){cout<<" One";}
+
     }
     return 0;
 }
